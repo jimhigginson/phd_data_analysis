fbf4f38ecdba6805a63391656911c6a4f9ac0c06
Jim Higginson <jah119@ic.ac.uk>
2022-11-14 20:13:47 +0000

    initial commit

---
+++ ms_data_class.py
@@ -0,0 +1,3 @@
+'''
+In this module I'm going to create (crib from my previous effort) and refine a class that can store the peak-picked data and metadata to allow easy repetition of data analysis
+'''
