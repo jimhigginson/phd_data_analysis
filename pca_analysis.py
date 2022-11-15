9bec1105fcd5b6ad00bf14e1b7800b8231d3e017
Jim Higginson <jah119@ic.ac.uk>
2022-11-15 15:31:47 +0000

    Initial commit of PCA plotting class

---
+++ pca_analysis.py
@@ -0,0 +1,55 @@
+'''
+Create a repeatable function using the peakpickeddata class to run a pca with scree plot, pcs and loadings to produce graphs and an output file
+
+Means everything can be done really repeatably
+'''
+import seaborn as sns
+from datetime import date
+from thesis_figure_parameters import tfParams
+
+class PeakPickingPCAPlotter():
+    '''
+    This class takes a PeakPickedData object, and generates a pdf report with the unsupervised analysis, showing a scree plot, PC plots demonstrating date and class relationships, then a loadings plot with identification of the biggest factors
+    '''
+    # Class variable
+    
+    pcs_to_plot = 5
+    today = date.today()
+    # for the corner plots, 5 is a sweet spot of informative but not too cluttered
+
+    def __init__(self, data_object): #maybe can import rcParams here to control the graphics centrally?
+        self.data = data_object
+        self.pc_plot_data = self.data.principal_components.iloc[:, 0:self.pcs_to_plot].join([self.data.binary_path, self.data.path, self.data.date])
+
+    def __str__(self):
+        return('Self encapsulated plotter function to allow easy repetition of PCA analysis')
+    # Do i want to change this so it programmatically describes the data I'm plotting?
+
+    def scree_plot(self):
+        self.ax = sns.barplot(x = self.data.pc_labels, y = self.data.pca.explained_variance_ratio_ * 100, color = 'k')
+        return(self.ax)
+
+
+    def binary_pc_plot(self):
+        '''
+        Returns a pretty corner graph of the PCs 1-5, coloured by binary tumour/non-tumour status of the tissue
+        '''
+        self.fig = sns.pairplot(data = self.pc_plot_data, hue='binary_path', corner=True, markers='.', height = tfParams['textwidth'], aspect = 1, plot_kws={'alpha':0.6, 'linewidth':0}, palette=['red','green'])
+        self.fig.savefig(f'{self.today}_binary_pc_plot.pdf')
+        return(self.fig)
+
+    def date_pc_plot(self):
+        '''
+        Returns a pretty corner graph of the PCs 1-5, coloured by date of analysis 
+        '''
+        self.ax = sns.pairplot(data = self.pc_plot_data, hue='date', corner=True, markers='.', plot_kws={'alpha':0.6, 'linewidth':0})
+        return(self.ax)
+
+    def multiclass_pc_plot(self):
+        '''
+        Returns a pretty corner graph of the PCs 1-5, coloured by class
+        '''
+        self.ax = sns.pairplot(data = self.pc_plot_data, hue='path', corner=True, markers='.', plot_kws={'alpha':0.6, 'linewidth':0})
+        return(self.ax)
+
+# Loadings plot
