c65c4c12ac902025a201a4f0dab1fdc8d4a6eeac
Jim Higginson <jah119@ic.ac.uk>
2022-11-15 15:31:28 +0000

    initial commit of centralised figure parameters

---
+++ thesis_figure_parameters.py
@@ -0,0 +1,8 @@
+'''
+In this file I will create dictionaries, variables and the like that set the sizes, colours and so on for all the figures to be included in the final thesis so that they are consistent
+'''
+
+tfParams = {
+    'textwidth': 5.17, #current textwidth (see thesis.pdf) is 13.123 cm / 5.17 inches
+    'textheight': 7.44 # as above
+        }
