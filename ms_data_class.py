a783132b2d00c0c3eb253c020a4f659466e382e4
Jim Higginson <jah119@ic.ac.uk>
2022-11-15 08:09:34 +0000

    added test

--- ms_data_class.py
+++ ms_data_class.py
@@ -1,3 +1,6 @@
 '''
 In this module I'm going to create (crib from my previous effort) and refine a class that can store the peak-picked data and metadata to allow easy repetition of data analysis
 '''
+
+
+TEST§
