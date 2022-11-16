d021593f9b536c413d99c4e6428e9fe59140ac64
Jim Higginson <jah119@ic.ac.uk>
2022-11-16 11:35:57 +0000

    started univariate class

--- pp_univariate_analysis.py
+++ pp_univariate_analysis.py
@@ -1,1 +1,20 @@
 print('Starting univariate analysis')
+
+class PeakPickingUnivariateAnalysis():
+    '''
+    This class takes a PeakPickedData object, and generates a volcano plot with a list of the most important features.
+    '''
+
+    # Class variables go here
+
+    def __init__(self, data_object):
+        self.data = data_object
+
+    def univariate_analysis():
+        # Univariate analysis goes here
+
+    def volcano_plot():
+        # Volcano plot goes here
+
+    def key_features():
+        # take the key features from univariate analysis and print them here.
