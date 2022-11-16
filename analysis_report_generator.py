a2c44eaaa1360682017b0dacbba84260b87e2cfe
Jim Higginson <jah119@ic.ac.uk>
2022-11-16 19:18:03 +0000

    added volcano plot to report generator

--- analysis_report_generator.py
+++ analysis_report_generator.py
@@ -1,14 +1,16 @@
 import pp_data_import 
 from ms_data_class import PeakPickedData
-# here import univariate analysis module I'll create eventually
+from pp_univariate_analysis import PeakPickingUnivariateAnalysis
 from pca_analysis import PeakPickingPCAPlotter
 
 print('Starting data analysis')
 print('Generating report')
 data = PeakPickedData(pp_data_import.data)
 pca = PeakPickingPCAPlotter(data)
+uni = PeakPickingUnivariateAnalysis(data)
 
 function_calls = [
+uni.volcano_plot,
 pca.scree_plot,
 pca.binary_pc_plot,
 pca.date_pc_plot,
