e2d8a087279e87fdd8b3ecc7fb7989138e7d6301
Jim Higginson <jah119@ic.ac.uk>
2022-11-17 13:30:30 +0000

    added modeller instances

--- analysis_report_generator.py
+++ analysis_report_generator.py
@@ -2,6 +2,7 @@ import pp_data_import
 from ms_data_class import PeakPickedData
 from pp_univariate_analysis import PeakPickingUnivariateAnalysis
 from pca_analysis import PeakPickingPCAPlotter
+from pp_supervised_rfecv import PeakPickModelBuilder
 
 print('Starting data analysis')
 print('Generating report')
@@ -19,7 +20,23 @@ pca.energy_device_pc_plot,
 pca.loadings_plot
 ]
 
+print('Starting univariate and unsupervised analysis')
 for i in function_calls:
     i()
+
+print('Univariate and unsupervised analysis complete')
+
+print('Starting multivariate model generation')
+print('Instantiating model builder class')
+
+modeller = PeakPickModelBuilder(data)
+
+binary_lda = modeller.binary_lda()
+multiclass_lda = modeller.multiclass_lda()
+binary_rf = modeller.binary_rf()
+multiclass_rf = modeller.multiclass_rf()
+
+
+
 
 print('Report generation complete')
