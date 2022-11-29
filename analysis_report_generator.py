b3fb89a7f7c10f7fef29d13918de647da392b559
Jim Higginson <jah119@ic.ac.uk>
2022-11-29 08:33:07 +0000

    created loop to refit and export features and models after rfecv

--- analysis_report_generator.py
+++ analysis_report_generator.py
@@ -42,5 +42,35 @@ binary_lda = modeller.binary_lda
 #binary_rf = modeller.binary_rf
 #multiclass_rf = modeller.multiclass_rf
 
+filenames = {
+    binary_lda : f'{today}_binary_lda_rfecv',
+    multiclass_lda : f'{today}_multiclass_lda_rfecv',
+    binary_rf : f'{today}_binary_rf_rfecv',
+    multiclass_rf : f'{today}_multiclass_rf_rfecv'
+        }
+
+for key, value in filenames.items():
+    filepath = f'{model_path}{value}.pkl'
+    print(f'Opening {filepath} for pickling')
+    file = open(filepath, 'wb')
+    print('##########################')
+    print(f'RFECV model for {key} had {key.n_features_} features')
+    print(f'Exporting features for {key}')
+    features = pd.DataFrame(key.get_feature_names_out())
+    features.to_csv(f'{model_path}{value}.csv')
+    print('Evaluating whether model was binary or multiclass for fitting with correct targets')
+    if len(key.classes_) == 2:
+        y = data.binary_path
+    else:
+        y = data.path
+    print(f'Fitting {key.estimator} with selected features for pickling')
+    model = key.estimator.fit(data.log_transform_data, y)
+    pickle.dump(model, file)
+    print(f'Re-fitted {key} pickled to {filepath}')
+    file.close()
+
+
+
+
 print(f'Report generation complete at {datetime.today()}.')
 print(f'Report generation took {datetime.today() - report_start}')
