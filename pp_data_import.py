04045deb5c94812111606e3dc6abe3df4e648c61
Jim Higginson <jah119@ic.ac.uk>
2023-01-10 06:48:16 +0000

    added binned data import

--- pp_data_import.py
+++ pp_data_import.py
@@ -6,9 +6,11 @@ import pandas as pd
 ##########
 
 data_path = './data/combined_data_metadata.csv'
+binned_data_path = './data/combined_binned_data_metadata.csv'
 
-print(f'Importing data from {data_path}')
+print(f'Importing peak-picked data from {data_path} and binned data from {binned_data_path}')
 
 data = pd.read_csv(data_path)
+binned_data = pd.read_csv(binned_data_path)
 
 print(f'Data successfully imported\n')
