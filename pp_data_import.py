deefb9ae3fc14d0f294d4527ca807bfa448ea6f4
Jim Higginson <jah119@ic.ac.uk>
2023-01-13 10:17:26 +0000

    added data to .gitignore

--- pp_data_import.py
+++ pp_data_import.py
@@ -5,12 +5,10 @@ import pandas as pd
 # path in rcs directory to ex-vivo peak-picked matrix
 ##########
 
-data_path = './data/combined_data_metadata.csv'
-binned_data_path = './data/combined_binned_data_metadata.csv'
+# data_path = './data/combined_data_metadata.csv'
+data_path = './data/combined_binned_data_metadata.csv'
 
-print(f'Importing peak-picked data from {data_path} and binned data from {binned_data_path}')
-
+print(f'Importing data from {data_path}') 
 data = pd.read_csv(data_path)
-binned_data = pd.read_csv(binned_data_path)
 
 print(f'Data successfully imported\n')
