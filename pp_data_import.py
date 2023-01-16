1a9969c9daea9d79216dabe0dc4cfceb0b73491a
Jim Higginson <jah119@ic.ac.uk>
2023-01-16 07:12:25 +0000

    changed back to pp data import

--- pp_data_import.py
+++ pp_data_import.py
@@ -5,8 +5,8 @@ import pandas as pd
 # path in rcs directory to ex-vivo peak-picked matrix
 ##########
 
-# data_path = './data/combined_data_metadata.csv'
-data_path = './data/combined_binned_data_metadata.csv'
+data_path = './data/combined_data_metadata.csv'
+#data_path = './data/combined_binned_data_metadata.csv'
 
 print(f'Importing data from {data_path}') 
 data = pd.read_csv(data_path)
