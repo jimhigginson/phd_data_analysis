1540f756afbfb4d5b46c2ebb0e078644df67600c
Jim Higginson <jah119@ic.ac.uk>
2023-02-17 21:41:30 +0000

    tidied up equivocal or missing samples

--- binned_data_import.py
+++ binned_data_import.py
@@ -9,5 +9,12 @@ data_path = './data/combined_binned_data_metadata.csv'
 
 print(f'Importing binned_data from {data_path}') 
 data = pd.read_csv(data_path)
+print('Dropping equivocal or missing samples')
+
+data = data[~data.path.isin(['Equivocal','No sample'])]
+
+print('Correcting "No tumour" to "Mucosa"')
+
+data.loc[data.path == 'No tumour', 'path'] = 'Mucosa'
 
 print(f'Binned data successfully imported\n')
