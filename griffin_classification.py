ba794435f758e211e3342cb9e4e6d798f4ee3262
Jim Higginson <jah119@ic.ac.uk>
2023-03-05 06:37:22 +0000

    Got animation working with mucosa/muscle only model

--- griffin_classification.py
+++ griffin_classification.py
@@ -19,6 +19,12 @@ from ms_data_class import BinnedData
 training_data = BinnedData(data)
 X = training_data.log_transform_data
 y = training_data.path
+
+print('Trimming training model data to exclude tumour')
+y = y[y!='Tumour']
+y[y=='Connective tissue'] = 'Mucosa'
+X = X.loc[y.index]
+
 print('Data import complete')
 
 print('Setting global variables')
@@ -85,29 +91,29 @@ g_metadata.loc[g_metadata['Sum.'] < threshold, 'prediction'] = 'No signal'
 colours.update({'No signal':'whitesmoke'})
 
 
+'''
 # Plot predictions on xy data
 print('Will eventually do a static plot here')
 print('Setting x, y and hue values')
 
-x_pos = g_metadata['x-position']
-y_pos = g_metadata['y-position']
+stretch_factor = 1.1
 
+g_metadata['x-position'] = g_metadata['x-position'] ** stretch_factor
+
+sns.scatterplot(data=g_metadata, x = 'x-position', y = 'y-position', hue = 'prediction', s=18)
+plt.show()
+'''
 print('Having a crack at animating')
 
 def animate(i):
-    plt.scatter(g_metadata.iloc[i]['x-position'], g_metadata.iloc[i]['y-position'], c=colours[g_metadata.iloc[i]['prediction']])
-    plt.xlim([0,10])
-    plt.ylim([0,10])
+    ax.scatter(g_metadata.iloc[i]['x-position'], g_metadata.iloc[i]['y-position'], c=colours[g_metadata.iloc[i]['prediction']], alpha = 0.8, s = 30)
+    ax.set_xlim((0,10))
+    ax.set_ylim((0,10))
     return(plt)
 
 fig = plt.figure()
+ax = plt.axes()
 print('Creating and saving animation')
-ani = FuncAnimation(fig, animate)
+ani = FuncAnimation(fig, animate, frames=870, interval=200)
 ani.save('basic_animation.mp4', fps=30, extra_args=['-vcodec', 'libx264'])
 
-
-# animate plot at 1hz
-
-# overlay animated plot with main video
-
-# Add other videos and photos if needed.
